c4cd46e43174048cfeb8995f35faab54ef4a6396
lbukowski85 <46279965+lbukowski85@users.noreply.github.com>
2020-10-13 19:27:44 -0300

    Habilitando AuthGuards do controller

--- back-end/src/imoveis/imovel.controller.ts
+++ back-end/src/imoveis/imovel.controller.ts
@@ -8,35 +8,35 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 export class ImovelController {
   constructor(private readonly imovelService: ImovelService) {}
 
-  // @UseGuards(AuthGuard(['default_user','master']))
+  @UseGuards(AuthGuard(['default_user','master']))
   @ApiBearerAuth()
   @Get('/:id')
   async get(@Param('id') id: string) {
       return await this.imovelService.get(id);
   }  
 
-  // @UseGuards(AuthGuard(['default_user','master']))
+  @UseGuards(AuthGuard(['default_user','master']))
   @ApiBearerAuth()
   @Get()
   async all() {
       return await this.imovelService.all();
   }  
 
-  // @UseGuards(AuthGuard('master'))
+  @UseGuards(AuthGuard('master'))
   @ApiBearerAuth()
   @Post()
   async create(@Body() create: CreateImovelDto) {
       return await this.imovelService.create(create);
   }
   
-  // @UseGuards(AuthGuard('master'))
+  @UseGuards(AuthGuard('master'))
   @ApiBearerAuth()
   @Put('/:id')
   async update(@Param('id') id: string, @Body() update: ImovelDto) {
       return await this.imovelService.update(id, update);
   }
 
-  // @UseGuards(AuthGuard('master'))
+  @UseGuards(AuthGuard('master'))
   @ApiBearerAuth()
   @Delete('/:id')
   async delete(@Param('id') id: string) {
